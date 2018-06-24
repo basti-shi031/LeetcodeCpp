fc99223e7283e90cf83a5326b2ec4e7f9e040969
basti <876572078@qq.com>
2018-06-24 16:47:18 +0800

    update

---
+++ Ex46_Permutations.cpp
@@ -0,0 +1,65 @@
+//
+// Created by Basti031 on 2018/6/24.
+//
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+//finished
+class Solution {
+public:
+    void generate(vector<int> nums, int index, vector<vector<int>> &result) {
+
+        if (index == nums.size()) {
+            return;
+        }
+
+        if (index == 0) {
+            vector<int> a;
+            a.push_back(nums[0]);
+            result.push_back(a);
+        } else {
+
+            int num = nums[index];
+
+            int size = result.size();
+            for (int i = 0; i < size; ++i) {
+                vector<int> item = result[i];
+
+                int itemSize = item.size();
+                for (int j = 0; j < itemSize + 1; ++j) {
+                    vector<int> temp = item;
+                    temp.insert(temp.begin() + j, num);
+                    result.push_back(temp);
+                }
+            }
+            result.erase(result.begin(), result.begin() + size);
+        }
+        generate(nums, index + 1, result);
+
+
+    }
+
+    vector<vector<int>> permute(vector<int> &nums) {
+        vector<vector<int>> result;
+        if (nums.size() == 0) {
+            return result;
+        }
+
+        generate(nums, 0, result);
+
+        return result;
+    }
+};
+
+int main() {
+    vector<int> a;
+    a.push_back(1);
+    a.push_back(2);
+    a.push_back(3);
+
+    Solution solution = Solution();
+    cout << solution.permute(a).size();
+}
