9ca6120bd3fd44dbb8d30f3e5b6fe42767d4dc8b
basti <876572078@qq.com>
2018-06-24 15:24:34 +0800

    update

--- Ex22_Generate_Parentheses.cpp
+++ Ex22_Generate_Parentheses.cpp
@@ -1,11 +1,44 @@
 #include<iostream>
 #include<vector>
+#include<string>
 
 using namespace std;
-//todo
+
+//finished
 class Solution {
 public:
-	vector<string> generateParenthesis(int n) {
 
-	}
+    void generate(int left, int right, string arg, vector<string>& result) {
+
+        if (left > right) return;
+        if (left == 0 && right == 0) {
+            result.push_back(arg);
+        }
+
+        if (left > 0) {
+            generate(left - 1, right, arg + "(", result);
+        }
+        if (right > 0) {
+            generate(left, right - 1, arg + ")", result);
+        }
+    }
+
+
+    vector<string> generateParenthesis(int n) {
+        vector<string> result;
+
+        if (n <= 0) {
+            return result;
+        }
+
+        generate(n, n, "", result);
+        return result;
+    }
 };
+
+/*int main() {
+    Solution solution = Solution();
+    vector<string> result = solution.generateParenthesis(3);
+    cout<<result.size();
+}*/
+
